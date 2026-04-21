b91376e2ad151b8d8be7627302882970d33b6487
Mohamed <modernkille3grow@gmail.com>
2026-04-21 11:12:35 +0300

    completed movies.jsx

---
+++ src/components/media/Movies.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
+
+const Movies = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await axios.get(
+          "https://popcorn-be.onrender.com/media/movies"
+        )
+        setMovies(res.data)
+      } catch (err) {
+        console.error("Error fetching movies:", err)
+      }
+    }
+    fetchMovies()
+  }, [])
+
+  return (
+    <div className="media-container">
+      <h1>Movies</h1>
+      <div className="media-grid">
+        {movies.map((movie) => (
+          <Link
+            to={`/movies/${movie._id}`}
+            key={movie._id}
+            className="media-card"
+          >
+            <img src={movie.image} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>Rating: {movie.rating}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Movies
