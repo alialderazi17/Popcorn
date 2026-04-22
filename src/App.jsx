5a3ddac8e0cd95ba431472ff0d27344e973f4f56
abeer-rozba <abeerrozba3@gmail.com>
2026-04-22 09:35:44 +0300

    minor fixes: removed h1 in movies and tv + moved Media.jsx into media folder and removed duplicate Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import MovieDetails from './components/media/MovieDetails'
 import TvDetails from './components/media/TvDetails'
-import Media from './components/Media'
+import Media from './components/media/Media.jsx'
 import Genre from './components/media/Genre.jsx'
 import GenreDetails from './components/media/GenreDetails.jsx'
 import Users from './components/pages/Users.jsx'
@@ -35,15 +35,15 @@ const App = () => {
   return (
     <>
       <Nav user={user} setUser={setUser} />
-      <div className='main-content'>
+      <div className="main-content">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login setUser={setUser} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
 
           <Route
-            path='/profile'
+            path="/profile"
             element={
               <ProtectedRoute user={user}>
                 <Profile user={user} />
@@ -51,13 +51,14 @@ const App = () => {
             }
           />
           <Route
-            path='/watchlist'
+            path="/watchlist"
             element={
               <ProtectedRoute user={user}>
                 <Watchlist user={user} />
               </ProtectedRoute>
             }
           />
+<<<<<<< Updated upstream
           <Route path='/watchlist/:id' element={<Watchlist user={user} />} />
           <Route path='/user' element={<Users />} />
           <Route path='/profile/:id' element={<Profile user={user} />} />
@@ -66,6 +67,14 @@ const App = () => {
           <Route path='/media' element={<Media />} />
           <Route path='/genres' element={<Genre />} />
           <Route path='/genres/:id' element={<GenreDetails />} />
+=======
+
+          <Route path="/movie/:id" element={<MovieDetails user={user} />} />
+          <Route path="/tv/:id" element={<TvDetails user={user} />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/genres" element={<Genre />} />
+          <Route path="/genres/:id" element={<GenreDetails />} />
+>>>>>>> Stashed changes
         </Routes>
       </div>
     </>
